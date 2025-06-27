0983069587cd0aaafd4aaf3bbfb5af336c49af00
zmohand <mohand.zeggane@outlook.fr>
2025-06-27 14:48:47 +0200

    Bug normalement trouvé, liste L non triée, clarification du code à poursuivre

--- graph.jl
+++ graph.jl
@@ -18,23 +18,8 @@ struct Graph
 end
 
 
-#= Implémentation de Bellman-Ford
-
-"""function Bellman_Ford(g :: Graph, s :: Vertex)
-    distance = fill(Inf64, (length(g.vertex_list), 0))
-    
-    distance[s.id] = 0
-
-    for k in 1:(length(g.vertex_list) - 1)
-        for 
-    end
-
-
-end=#
-
-
 # Fonction qui va vérifier si une réduction donnée (f_c, l_c) est possible 
-function is_curtailment_possible(t_max::Int64, first_curtailment::Int64, last_curtailment::Int64, delta_min::Int64, delta_max::Int64)
+function is_curtailment_possible(; t_max::Int64, first_curtailment::Int64, last_curtailment::Int64, delta_min::Int64, delta_max::Int64)
     # Je separe chaque cas où la reduction n'est pas possible (c'est plus visible)
     if (first_curtailment > last_curtailment)
         return false
@@ -57,14 +42,14 @@ end
 
 
 #Fonction qui va génerer l'ensemble des sommets du graphe
-function init_curtailments(t_max::Int64, delta::Int64, delta_min::Int64, delta_max::Int64, discharge_levels::Vector{Float64})
+function init_curtailments(; t_max::Int64, delta::Int64, delta_min::Int64, delta_max::Int64, discharge_levels::Vector{Float64})
     vertex_array = Array{Vertex}(undef, 0)
     sequence_id = 1
 
     # Parcours tous les curtailments possibles (avec niveau de décharge) et les rajoute dans l'ensemble de sommets
     for first_c = 1:t_max
         for last_c = first_c:t_max
-            if is_curtailment_possible(t_max, first_c, last_c, delta_min, delta_max)
+            if is_curtailment_possible(t_max = t_max, first_curtailment = first_c, last_curtailment = last_c, delta_min = delta_min, delta_max = delta_max)
                 for d_lvl in discharge_levels
                     push!(vertex_array, Vertex(sequence_id, first_c, last_c, d_lvl))
                     sequence_id = sequence_id + 1
@@ -79,12 +64,12 @@ end
 
 
 #fonction qui calcule tous les levels discharge possibles
-function load_discharge_levels(b_max::Float64, b_min::Float64, discharge_precision::Int64)
+function load_discharge_levels(; b_max::Float64, b_min::Float64, discharge_precision::Int64)
 
     discharge_array = Array{Float64}(undef, 0)
     level = discharge_precision/100
 
-    while (level < 1)
+    while (level <= 1)
         curr_lvl = round(level*b_max)
 
         # Si cette décharge est bien "possible" on la met dans l'array
@@ -104,13 +89,10 @@ end
 
 
 # Fonction qui va calculer le t_c_b d'une réduction c en utilisant la "property 1" 
-function calcul_t_c_b(t_max::Int64, c::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64})
+function calcul_t_c_b(; t_max::Int64, c::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64})
     t_c_b = c.curtailment_end+1
-    # On vérifie qu'on ne sort pas du time horizon
-    if t_c_b > t_max
-        return -1
-    end
-    reference_value = delta*sum(min(p_b, p_max - w[t]) for t = (c.curtailment_end+1):t_c_b)
+    
+    reference_value = delta * sum(min(p_b, p_max - w[t]) for t in (c.curtailment_end+1):t_c_b)
 
     while reference_value < c.discharge
         t_c_b = t_c_b + 1
@@ -118,7 +100,7 @@ function calcul_t_c_b(t_max::Int64, c::Vertex, delta::Int64, p_b::Float64, p_max
         if t_c_b > t_max
             return -1
         end
-        reference_value = delta * sum((min(p_b, p_max - w[t]) for t = (c.curtailment_end+1):t_c_b); init=0.0)
+        reference_value = delta * sum(min(p_b, p_max - w[t]) for t in (c.curtailment_end+1):t_c_b)
     end
 
     #println("Valeur retournée par t_c_b = ",t_c_b, " appel sur curtailment commencant à ", c.curtailment_start, " et finissant à ", c.curtailment_end)
@@ -127,8 +109,8 @@ function calcul_t_c_b(t_max::Int64, c::Vertex, delta::Int64, p_b::Float64, p_max
 end
 
 # Fonction qui va calculer r_c (la quantité d'energie achetée à la derniere periode de recharge)
-function calcul_r_c(c::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, t_max::Int64)
-    t_c_b = calcul_t_c_b(t_max, c, delta, p_b, p_max, w)
+function calcul_r_c(; c::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, t_max::Int64)
+    t_c_b = calcul_t_c_b(t_max = t_max, c = c, delta = delta, p_b = p_b, p_max = p_max, w = w)
     return (c.discharge/delta) - sum(min(p_b, p_max - w[t]) for t = (c.curtailment_end+1):(t_c_b - 1); init= 0.0)
 
 end
@@ -137,14 +119,14 @@ end
 d'energie pour recharger la batterie avant le début du curtailment c2, 
 peut dépendre du curtailment précendent c1
 =#
-function calcul_u_b_f_c_minus_one(c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, t_max::Int64)
-    t_c1_b = calcul_t_c_b(t_max, c1, delta, p_b, p_max, w)
+function calcul_u_b_f_c_minus_one(; c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, t_max::Int64)
+    t_c1_b = calcul_t_c_b(t_max = t_max, c = c1, delta = delta, p_b = p_b, p_max = p_max, w = w)
     
     # Cas où on le c2 ne dépend pas de c1, pas de corrélation
     if (t_c1_b < (c2.curtailment_start - 1))
         return 0
     else
-        return calcul_r_c(c1, delta, p_b, p_max, w, t_max)
+        return calcul_r_c(c = c1, delta = delta, p_b = p_b, p_max = p_max, w = w, t_max = t_max)
     end
 
 end
@@ -152,15 +134,15 @@ end
 
 
 # Fonction qui va calculer le P_c_j_max de la reduction c_j qui vient après c_i en utilisant le lemme 1 et propriete 1
-function calcul_p_c_j_max(c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, t_max::Int64, dummy::Bool)
+function calcul_p_c_j_max(; c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, t_max::Int64, dummy::Bool)
 
     # Si on traite un sommet factice :
     if dummy
         u_f_c_minus_1 = 0
     else
-        u_f_c_minus_1 = w[c2.curtailment_start-1] + calcul_u_b_f_c_minus_one(c1, c2, delta, p_b, p_max, w, t_max)
+        u_f_c_minus_1 = w[c2.curtailment_start-1] + calcul_u_b_f_c_minus_one(c1 = c1, c2 = c2, delta = delta, p_b = p_b, p_max = p_max, w = w, t_max = t_max)
     end
-    omega_c = (sum(w[t] for t = c2.curtailment_start:c2.curtailment_end) + u_f_c_minus_1) / (c2.curtailment_end - c2.curtailment_start + 2)
+    omega_c = (sum(w[t] for t in c2.curtailment_start:c2.curtailment_end) + u_f_c_minus_1) / (c2.curtailment_end - c2.curtailment_start + 2)
 
     #println("Pcj max achetable = ", max(0, omega_c-p_TSO), " avec c1 commence à ", c1.curtailment_start, " et finissant à ", c1.curtailment_end, " et c2 commencant à ", c2.curtailment_start, " et finissant à ", c2.curtailment_end, " et discharge c1: ", c1.discharge, " et celle de c2 :", c2.discharge)
     return max(0, omega_c - p_TSO)
@@ -169,11 +151,14 @@ end
 
 
 # Fonction qui calcule la borne sup et la borne inf de d_c_j (qui commence après d_c_i)
-function calcul_bornes_d_c_j(c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, discharge_min::Float64, discharge_max::Float64, b_max::Float64, b_min::Float64, t_max::Int64)
-    p_c_j_max = calcul_p_c_j_max(c1, c2, delta, p_b, p_max, w, p_TSO, t_max, false)
-    borne_inf = sum((delta*max(min(w[t], discharge_min), w[t] - p_c_j_max)) for t = c2.curtailment_start:c2.curtailment_end)
+function calcul_bornes_d_c_j(; c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, discharge_min::Float64, discharge_max::Float64, b_max::Float64, b_min::Float64, t_max::Int64)
+    p_c_j_max = calcul_p_c_j_max(c1 = c1, c2 = c2, delta = delta, p_b = p_b, p_max = p_max, w = w, p_TSO = p_TSO, t_max = t_max, dummy = false)
+    borne_inf = sum(delta * max(min(w[t], discharge_min), w[t] - p_c_j_max) for t in c2.curtailment_start:c2.curtailment_end)
 
-    borne_sup = min(sum(delta*min(w[t], discharge_max) for t = c2.curtailment_start:c2.curtailment_end), b_max - b_min)
+    borne_sup = min(
+        sum(delta * min(w[t], discharge_max) for t in c2.curtailment_start:c2.curtailment_end),
+        b_max - b_min
+    )
 
     #println("Bornes inf =", borne_inf, " et borne sup :", borne_sup)
     return borne_inf, borne_sup
@@ -181,9 +166,9 @@ end
 
 
 # Fonction qui va vérifier si un arc entre deux sommets est possible
-function is_arc_possible(t_max::Int64, c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, discharge_min::Float64, discharge_max::Float64, b_max::Float64, b_min::Float64)
+function is_arc_possible(; t_max::Int64, c1::Vertex, c2::Vertex, delta::Int64, p_b::Float64, p_max::Float64, w::Array{Float64}, p_TSO::Float64, discharge_min::Float64, discharge_max::Float64, b_max::Float64, b_min::Float64)
    
-    t_c1_b = calcul_t_c_b(t_max, c1, delta, p_b, p_max, w)
+    t_c1_b = calcul_t_c_b( t_max = t_max, c = c1, delta = delta, p_b = p_b, p_max = p_max, w = w)
     # Cas où on sort des bornes
     if t_c1_b == -1
         return false
@@ -195,7 +180,7 @@ function is_arc_possible(t_max::Int64, c1::Vertex, c2::Vertex, delta::Int64, p_b
     end
 
     # Il faut maintenant calculer les bornes sup et inf pour d_c_2
-    borne_inf, borne_sup = calcul_bornes_d_c_j(c1, c2, delta, p_b, p_max, w, p_TSO, discharge_min, discharge_max, b_max, b_min, t_max)
+    borne_inf, borne_sup = calcul_bornes_d_c_j(c1 = c1, c2 = c2, delta = delta, p_b = p_b, p_max = p_max, w = w, p_TSO = p_TSO, discharge_min = discharge_min, discharge_max = discharge_max, b_max = b_max, b_min = b_min, t_max = t_max)
     if c2.discharge < borne_inf || c2.discharge > borne_sup
         return false
     end
@@ -207,11 +192,11 @@ function is_arc_possible(t_max::Int64, c1::Vertex, c2::Vertex, delta::Int64, p_b
 end
 
 # Fonction qui calcule le f_c_B (cout de recharge de la batterie)
-function calcul_battery_charge_cost(energy_cost::Array{Float64}, t_max::Int64, delta::Int64, curtailment::Vertex, p_b::Float64, p_max::Float64, w::Array{Float64})
+function calcul_battery_charge_cost(; energy_cost::Array{Float64}, t_max::Int64, delta::Int64, curtailment::Vertex, p_b::Float64, p_max::Float64, w::Array{Float64})
 
     somme = 0
-    t_c_b = calcul_t_c_b(t_max, curtailment, delta, p_b, p_max, w)
-    for i = (curtailment.curtailment_end+1):(t_c_b - 1)
+    t_c_b = calcul_t_c_b(t_max = t_max, c = curtailment, delta = delta, p_b = p_b, p_max = p_max, w = w)
+    for i in (curtailment.curtailment_end+1):(t_c_b - 1)
         somme = somme + (energy_cost[i] - energy_cost[t_c_b])*min(p_b, p_max - w[i])
     end
     #println("Prob t_c_b ?", t_c_b, " t_max : ", t_max, " c_start :", curtailment.curtailment_start, " c_end : ", curtailment.curtailment_end)
@@ -220,16 +205,22 @@ end
 
 
 # Fonction qui calcule G'(t) MODE OTR(TODO), ref page 982 pour changer en mode FTR
-function calcul_g_prime(energy_cost::Array{Float64}, reward::Array{Float64}, delta::Int64, time::Int64)
-    return (energy_cost[time] + reward[time])/delta
+function calcul_g_prime(; energy_cost::Array{Float64}, reward::Array{Float64}, delta::Int64, time::Int64, OTR::Bool)
+    if OTR
+        return (energy_cost[time] + reward[time])/delta
+    else
+        return 0 # TODO FTR
+    end
 end
 
 # Fonction qui va calculer l'ensemble L (ref preuve théorème 1 page 982)
-function calcul_array_L(energy_cost::Array{Float64}, reward::Array{Float64}, delta::Int64, curtailment::Vertex)
+function calcul_array_L(; energy_cost::Array{Float64}, reward::Array{Float64}, delta::Int64, curtailment::Vertex)
     L_temp = Vector{Tuple{Float64, Int64}}()
     for i = curtailment.curtailment_start:curtailment.curtailment_end
-        push!(L_temp, (calcul_g_prime(energy_cost, reward, delta, i), i))
+        push!(L_temp, (calcul_g_prime(energy_cost = energy_cost, reward = reward, delta = delta, time = i), i))
     end
+    
+    sort!(L_temp; by = first, rev = true)
 
     return [i for (_, i) in L_temp]
 
@@ -237,7 +228,7 @@ end
 
 
 # Fonction qui va calculer le d_t_min
-function d_t_min(delta::Int64, time::Int64, w::Array{Float64}, discharge_min::Float64, p_c_max::Float64)
+function d_t_min(; delta::Int64, time::Int64, w::Array{Float64}, discharge_min::Float64, p_c_max::Float64)
     return delta*max(min(w[time], discharge_min), w[time] - p_c_max)
 end
 
